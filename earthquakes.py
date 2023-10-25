381f25511ca2f38d668f3e93124be630d3e3fbd0
Joonmin <zcemjh1@ucl.ac.uk>
2023-10-25 16:43:12 +0100

    mag/coords of max

--- earthquakes.py
+++ earthquakes.py
@@ -3,7 +3,7 @@
 # However, we will use a more powerful and simpler library called requests.
 # This is external library that you may need to install first.
 import requests
-
+import json
 
 def get_data():
     # With requests, we can ask the web service for the data.
@@ -24,34 +24,53 @@ def get_data():
     # The response we get back is an object with several fields.
     # The actual contents we care about are in its text field:
     text = response.text
+
+    #with open('text.json', 'w') as json_file:
+
+    #    json.dump(text, json_file, indent=4)
+
+    #with open('text.json', 'r') as json_file:
+    #    text_string = json_file.read()
+
+    #print(text_string)
+
     # To understand the structure of this text, you may want to save it
     # to a file and open it in VS Code or a browser.
     # See the README file for more information.
-    ...
+    dic = json.loads(text)
+
 
     # We need to interpret the text to get values that we can work with.
     # What format is the text in? How can we load the values?
-    return ...
+    return dic
 
 def count_earthquakes(data):
-    """Get the total number of earthquakes in the response."""
-    return ...
+    
+    return data["metadata"]["count"]
 
 
 def get_magnitude(earthquake):
     """Retrive the magnitude of an earthquake item."""
-    return ...
+    return earthquake["properties"]["mag"]
 
 
 def get_location(earthquake):
     """Retrieve the latitude and longitude of an earthquake item."""
     # There are three coordinates, but we don't care about the third (altitude)
-    return ...
+    return earthquake['geometry']['coordinates'][:2]
 
 
 def get_maximum(data):
-    """Get the magnitude and location of the strongest earthquake in the data."""
-    ...
+    max = 0
+    location = 0 
+
+    # Iterate through the earthquake data to find the earthquake with the maximum magnitude.
+    for i in data['features']:
+        if get_magnitude(i) > max:
+            max = get_magnitude(i)
+            location = get_location(i)
+
+    return max, location
 
 
 # With all the above functions defined, we can now call them and get the result
