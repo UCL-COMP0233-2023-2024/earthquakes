58ac52f00fa99e37efc9a36af0464ef9b448db7f
Sophie Morrison <sophie.morrison.23@ucl.ac.uk>
2023-10-26 11:27:08 +0100

    added plot avg mag per yr

--- outline.py
+++ outline.py
@@ -1,11 +1,38 @@
+import requests
+import json
 from datetime import date
+import numpy as np
 
 import matplotlib.pyplot as plt
 
 
 def get_data():
-    """Retrieve the data we will be working with."""
-    ...
+    # With requests, we can ask the web service for the data.
+    # Can you understand the parameters we are passing here?
+    response = requests.get(
+        "http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
+        params={
+            'starttime': "2000-01-01",
+            "maxlatitude": "58.723",
+            "minlatitude": "50.008",
+            "maxlongitude": "1.67",
+            "minlongitude": "-9.756",
+            "minmagnitude": "1",
+            "endtime": "2018-10-11",
+            "orderby": "time-asc"}
+    )
+
+
+    # The response we get back is an object with several fields.
+    # The actual contents we care about are in its text field:
+    text = response.text
+    # To understand the structure of this text, you may want to save it
+    # to a file and open it in VS Code or a browser.
+    # See the README file for more information.
+
+    # We need to interpret the text to get values that we can work with.
+    # What format is the text in? How can we load the values?
+    return json.loads(text)
 
 
 def get_year(earthquake):
@@ -22,7 +49,8 @@ def get_year(earthquake):
 
 def get_magnitude(earthquake):
     """Retrive the magnitude of an earthquake item."""
-    ...
+    return earthquake["properties"]["mag"]
+
 
 
 # This is function you may want to create to break down the computations,
@@ -32,11 +60,33 @@ def get_magnitudes_per_year(earthquakes):
     
     Returns a dictionary with years as keys, and lists of magnitudes as values.
     """
-    ...
+    dictionary = {}
 
+    for item in earthquakes:
+        year = get_year(item)
+        magnitude = get_magnitude(item)
+
+        if year not in dictionary:
+            dictionary[year] = []
+
+        dictionary[year].append(magnitude)
+
+    return dictionary
 
 def plot_average_magnitude_per_year(earthquakes):
-    ...
+    plot_data = get_magnitudes_per_year(earthquakes)
+    years = []
+    year_av = []
+
+    for year in plot_data:
+        years.append(year)
+        year_av.append(np.mean(plot_data[year]))
+
+    plt.plot(years, year_av)
+    plt.title("Average Magnitude per Year")
+    plt.xlabel('Year')
+    plt.ylabel('Magnitude')
+    plt.show()
 
 
 def plot_number_per_year(earthquakes):
@@ -45,7 +95,9 @@ def plot_number_per_year(earthquakes):
 
 
 # Get the data we will work with
+data = get_data()
 quakes = get_data()['features']
+print(get_magnitudes_per_year(quakes))
 
 # Plot the results - this is not perfect since the x axis is shown as real
 # numbers rather than integers, which is what we would prefer!
