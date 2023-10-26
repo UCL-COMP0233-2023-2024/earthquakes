b1bfdc2c69f0ef87e00c5689ae97388664a3d910
Tianyang Xie <994220475@qq.com>
2023-10-26 09:26:45 +0100

    change the coordinate

--- earthquakes.py
+++ earthquakes.py
@@ -56,7 +56,7 @@ def get_magnitude(earthquake):
 def get_location(earthquake):
     """Retrieve the latitude and longitude of an earthquake item."""
     # There are three coordinates, but we don't care about the third (altitude)
-    return earthquake["geometry"]["coordinates"]
+    return (earthquake["geometry"]["coordinates"][0],earthquake["geometry"]["coordinates"][1])
 
 
 def get_maximum(data):
