41fd5ebdcd745fe7302f819ba9c03408a2267528
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-11-11 15:27:47 +0000

    Update earthquakes.py

--- earthquakes.py
+++ earthquakes.py
@@ -2,6 +2,7 @@
 # over the Internet.
 # However, we will use a more powerful and simpler library called requests.
 # This is external library that you may need to install first.
+
 import requests
 
 
@@ -27,31 +28,38 @@ def get_data():
     # To understand the structure of this text, you may want to save it
     # to a file and open it in VS Code or a browser.
     # See the README file for more information.
-    ...
+    with open("earthquakes.json", "w") as f:
+        f.write(text)
 
     # We need to interpret the text to get values that we can work with.
     # What format is the text in? How can we load the values?
-    return ...
+    return json.loads(text)
 
 def count_earthquakes(data):
     """Get the total number of earthquakes in the response."""
-    return ...
+    return len(data["features"])
 
 
 def get_magnitude(earthquake):
     """Retrive the magnitude of an earthquake item."""
-    return ...
+    return earthquake["properties"]["mag"]
 
 
 def get_location(earthquake):
     """Retrieve the latitude and longitude of an earthquake item."""
     # There are three coordinates, but we don't care about the third (altitude)
-    return ...
+    return earthquake["geometry"]["coordinates"][:2]
 
 
 def get_maximum(data):
     """Get the magnitude and location of the strongest earthquake in the data."""
-    ...
+    current_max = 0
+    current_loc = None
+    for earthquake in data["features"]:
+        if magnitude := get_magnitude(earthquake) > current_max:
+            current_max = magnitude
+            current_loc = get_location(earthquake)
+    return current_max, current_loc
 
 
 # With all the above functions defined, we can now call them and get the result
